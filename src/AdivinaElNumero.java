6cad5e6c29db2ec8f783354f960f6a337440a610
NaudelynLucena <naulunau@gmail.com>
2024-09-23 15:27:03 +0200

    cambio de turno

--- src/AdivinaElNumero.java
+++ src/AdivinaElNumero.java
@@ -2,25 +2,37 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AdivinaElNumero{
+    public static boolean esValido(String s){
+        int n;
+        try {
+            n = Integer.parseInt(s);
+            if(n < 101|| n > -1){
+                return true;
+            } else {return false;}
+            
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
     public static void main(String[] args) {
-        int numeroSecreto = new Random().nextInt(0, 101);
+        String numeroSecreto = String.valueOf (new Random().nextInt(0, 101));
         int numeroIntentos = 0;
-        int input = -2;
+        String input = "";
         System.out.println("Juego de adivinar el numero");
         System.out.println("Introduzca un numero entre 0 y 100");
         while(input!=numeroSecreto){
-            if(input!=-2){
+            if(! input.equals("")){
                 System.out.println("Prueba otra vez");
             }
-            input = new Scanner(System.in).nextInt();
-            while(input<0||input>100){
+            input = new Scanner(System.in).next();
+            while(! esValido(input)){
                 System.out.println("Input no valido. Introduzca un numero entre 0 y 100");
-                input = new Scanner(System.in).nextInt();
+                input = new Scanner(System.in).next();
             }
             numeroIntentos++;
-            if(input>numeroSecreto){
+            if(Integer.valueOf(input)> Integer.valueOf(numeroSecreto)){
                 System.out.println("El numero secreto es mas pequeño");
-            }else if (input<numeroSecreto) {
+            }else if (Integer.valueOf(input)< Integer.valueOf(numeroSecreto)) {
                 System.out.println("El numero secreto es mas grande");
             }
         }
